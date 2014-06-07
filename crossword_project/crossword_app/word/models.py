ab716bd2294f7f3e0662145266d121a4f02de41b
Dawid Kostyszak <d.kostyszak@gmail.com>
2014-06-08 00:28:27 +0200

    add new models

--- crossword_project/crossword_app/word/models.py
+++ crossword_project/crossword_app/word/models.py
@@ -1,7 +1,25 @@
 from django.db import models
 
 
+class Category(models.Model):
+    category = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.category
+
+
+class Question(models.Model):
+    question = models.TextField(max_length=150)
+    categories = models.ForeignKey(Category)
+
+    def __str__(self):
+        return self.question
+
+
 class Word(models.Model):
     name = models.CharField(max_length=30)
-    question = models.TextField(max_length=150)
-    category = models.CharField(max_length=20)
+    length = models.IntegerField()
+    question = models.ForeignKey(Question)
+
+    def __str__(self):
+        return self.name
