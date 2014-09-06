1bb7368d61c4b9ccaab47761e1eeda4ba10d5815
Dawid Kostyszak <d.kostyszak@gmail.com>
2014-09-06 15:28:02 +0200

    placeholder for crossword

--- crossword_project/crossword_app/urls.py
+++ crossword_project/crossword_app/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-from .views import HomepageView
+from .views import HomepageView, CrosswordGenerate
 
 admin.autodiscover()
 
@@ -12,6 +12,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', HomepageView.as_view(), name="home"),
+    url(r'^crossword/', CrosswordGenerate.as_view(), name="crossword"),
     (r'^accounts/', include('crossword_app.auth.urls', app_name='authorization')),
     (r'^word/', include('crossword_app.word.urls', app_name='word')),
 )
--- crossword_project/crossword_app/views.py
+++ crossword_project/crossword_app/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
 from django.http import HttpResponse
@@ -15,3 +16,17 @@ class LoginRequiredMixin(object):
     @method_decorator(login_required)
     def dispatch(self, request, *args, **kwargs):
         return super(LoginRequiredMixin, self).dispatch(request, *args, **kwargs)
+
+
+class CrosswordGenerate(LoginRequiredMixin, TemplateView):
+    template_name = "generate_crossword.html"
+
+    def get(self, request, *args, **kwargs):
+        crossword = [
+            ['', '', '', 'K', 'A', 'R', 'O', 'L'],
+            ['', 'A', 'D', 'A', 'M', '', '', ''],
+            ['M', 'A', 'D', 'Z', 'I', 'A', '', ''],
+            ['', '', '', 'I', 'G', 'A', '', ''],
+            ['', '', '', 'O', 'L', 'A', 'F', '']
+        ]
+        return render(request, self.template_name, {'crossword': crossword})
